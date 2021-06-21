dce202ffac53a1b4a3ae4069bb66e60a8321162b
julieconnors <juliana.g.connors@gmail.com>
2021-06-21 06:09:11 -0400

    commit tree 414c3f0c48ee63ecd3861d948687691aa9be6366

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-// Code your solution here!
+function printString(myString) {
+	console.log(myString[0]);
 
+	if (myString.length > 1) {
+		let mySubString = myString.substring(1, myString.length);
+		printString(mySubString);
+	} else {
+		return true;
+	}
+}
+
+function reverseString(string) {
+    if (string === "") {
+        return "";
+    } else {
+        return reverseString(string.substring(1)) + string.charAt(0)
+    }
+}
+
+function isPalindrome(string) {
+    if (string[0] === string[string.length - 1] && string.length > 1) {
+        isPalindrome(string.substring(1, string.length -1)) 
+        return true
+      } else {
+        return false
+    }
+}
+
+function addUpTo(arr, i) {
+    return i ? arr[i] + addUpTo(arr, --i) : arr[i];
+
+    // if (i > 0) {
+    //     arr[i] + addUpTo(arr, --i)
+    //     // addUpTo(arr.slice(0, i-1) + arr.pop())
+    // } else {
+    //     return arr[0];
+    // }
+}
+
+function maxOf(arr) {
+    if (arr.length === 1) {
+        return arr[0]
+    } else {
+        return Math.max(arr.pop(), maxOf(arr));
+    }
+}
+
+function includesNumber(arr, n) {
+    if (!arr.length) {
+        return false
+    } else if (arr[0] === n) {
+        return true
+    } else {
+        return includesNumber(arr.slice(1), n)
+    }
+}
